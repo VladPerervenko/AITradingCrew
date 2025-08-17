49d68612b9fd8360b1dc8cb7ad912485afdf90cd
VladPerervenko <vldminkov@gmail.com>
2025-08-17 16:10:06 +0300

    refactor: Обновление и очистка проекта

--- test_deep.sh
+++
@@ -1,9 +0,0 @@
-curl -X POST https://openrouter.ai/api/v1/chat/completions \
--H "Authorization: Bearer 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" \
--H "Content-Type: application/json" \
--d '{
-    "model": "deepseek/deepseek-r1:free",
-    "messages": [
-      {"role": "user", "content": "Hello, how are you?"}
-    ],
-    "max_tokens": 100,}'
